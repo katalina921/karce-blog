a9aaaafd332eb67e7c6ed3a5fa88fe7cc8719eb7
Katalina  Arce <katalinaarce34@gmail.com>
2015-02-24 08:56:09 -0800

    finished all videos

--- controller/login-user.php
+++ controller/login-user.php
@@ -10,7 +10,8 @@ $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERRE
 if($query->num_rows == 1){
     $row = $query->fetch_array();
     
-    if($row["password"] == crypt($password, $row["salt"])){
+    if($row["password"] === crypt($password, $row["salt"])){
+       $_SESSION["authenticated"] = true;
         echo "<p>Login Successful!</p>";
     }
     else{
