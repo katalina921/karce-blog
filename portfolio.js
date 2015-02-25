a6f4ba75030d663656056375db25f28c82b6c1c8
Katalina  Arce <katalinaarce34@gmail.com>
2015-02-25 08:28:51 -0800

    linked blog

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+
+//accordion stuff
+
+$("document").ready(function() { 
+     $('#superHuman').accordion({header: "h4"});
+     $(".Carrot").css("color", "blue"); 
+     $(".Rabbit").css("color","gold"); 
+     $('#bgvid').css("width", "1000");
+          $('#bgvid').css("height", "1000");
+
+ });
+ 
+ $("document").ready(function(){
+     $("#nav nav-tabs").css("color, red");
+ });
+     
